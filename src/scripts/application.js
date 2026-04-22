f18acf287815edd238687477c75e6427f31ce4fd
tducn-student <duc.nguyen240205@vnuk.edu.vn>
2026-04-23 06:21:56 +0700

    feat: implement core portfolio UI sections (banner, about, contact)

---
+++ src/scripts/application.js
@@ -0,0 +1,5 @@
+import '../css/style.css'
+import initScrollReveal from './scroll-reveal';
+
+// Initialize ScrollReveal
+initScrollReveal();
---
+++ src/scripts/scroll-reveal.js
@@ -0,0 +1,48 @@
+import ScrollReveal from 'scrollreveal'
+
+export default function initScrollReveal(defaultProps = null) {
+    const targetElements = [
+        {
+            element: ".banner-text",
+            animation: {
+                delay: 700,
+                origin: window.innerWidth > 768 ? "left" : "bottom",
+            },
+        },
+        {
+            element: ".banner-cta",
+            animation: {
+                delay: 1000,
+                origin: window.innerWidth > 768 ? "left" : "bottom",
+            },
+        },
+        {
+            element: ".section-title",
+            animation: {
+                delay: 300,
+                distance: "0px",
+                origin: "bottom",
+            },
+        },
+        {
+            element: ".section-content",
+            animation: {
+                delay: 500,
+                distance: "0px",
+                origin: "bottom",
+            },
+        },
+    ];
+    
+    ScrollReveal({ 
+        reset: false,
+        distance: '60px',
+        duration: 1000,
+        delay: 200,
+        easing: 'cubic-bezier(0.5, 0, 0, 1)'
+    });
+    
+    targetElements.forEach(({ element, animation }) => {
+        ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
+    });
+}
